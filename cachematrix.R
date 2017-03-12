ad6ea2b2ac861ea40e0daf5130e349fa4eff0e62
Mark Barkell <markbarkell@gmail.com>
2017-03-12 14:54:27 -0400

    Implemented the assignment to provide a container object that is able to hold a cache to an inverse of a matrix.  In this way, do not need to recalculate the inverse over and over again.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programmer: Mark Barkell
+## Date Created:  2017-03-12 (See version history in git for changes)
+##
+## The makeCacheMatrix and the cacheSolveMatrix are implemented to store after calculating
+## a matrix inverse so that the stored matrix inverse may be used to trade RAM space for 
+## lower times in calculation.
+##
+## Please Note, the implementation of this functionality leans very much upon
+## the example code which is part of the assignment.   That example code
+## caches mean data.  The big difference here is mainly just variable and
+## and function names.  The general grammatical pattern of the functions
+## is as the course indicates in the README.md
 
-## Write a short comment describing this function
+## Returns an instance of an object set up to save or get a matrix inversion
+## into memory.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ci <- NULL
+  set <- function(y) {
+    x <<- y
+    ci <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inv) ci <<- inv
+  getinverse <- function() ci
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Retrive from cache, or calculate and cache, the inverse of the matrix x.
+## Precondition, x must be invertable.
+## parameters:  x, required: matrix to invert.
+##              ... addition values to pass to the solve() function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    ci <- x$getinverse()
+    if (!is.null(ci)) {
+      print("getting cached data")
+      return (ci)
+    } 
+    else {
+      print("no cache to serve")
+    }
+    d <- x$get()
+    ci <- solve(d, ...)
+    x$setinverse(ci)
+    ci
 }
